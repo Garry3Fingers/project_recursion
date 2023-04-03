b4306a4e68ec29d10b0967499e9e63430393fe24
Garry3Fingers <www.vlad78@yandex.ru>
2023-04-03 22:19:16 +0300

    Implement the fibs method

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+def fibs(num)
+  i = 1
+  arr = [0]
+  arr << 1 if num.positive?
+
+  while i < num
+    arr << 1 if i == 1
+    arr << arr[-2] + arr[-1] if arr.length >= 3 && arr.length < num + 1
+    i += 1
+  end
+
+  arr
+end
+
+p fibs(0)
+p fibs(1)
+p fibs(2)
+p fibs(8)
