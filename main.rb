27cf86c276f0c50cf1b96dab93e4864660c0a0ae
Garry3Fingers <www.vlad78@yandex.ru>
2023-04-04 15:24:57 +0300

    Refactor fibs

--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 def fibs(num)
-  i = 1
-  arr = [0]
-  arr << 1 if num.positive?
+  return [0] if num.zero?
 
+  arr = [0, 1]
+  return arr if num == 1
+
+  i = 1
   while i < num
-    arr << 1 if i == 1
-    arr << arr[-2] + arr[-1] if arr.length >= 3 && arr.length < num + 1
+    arr << arr[-2] + arr[-1]
     i += 1
   end
 
